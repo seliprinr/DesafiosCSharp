cb06e6ce1624af1b9075723ecba69b54b1c8ae5f
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-24 23:00:23 -0300

    Desafio em C#.

--- DesafiosCSharp/Program.cs
+++ DesafiosCSharp/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Digite sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade != 60)
+{
+    Console.WriteLine("Você ainda não tem o direito a tirar o cartão idoso da prefeitura");
+    int anosfaltantes = 60 - idade;
+    int diasfaltantes = anosfaltantes * 365;
+    Console.WriteLine("Faltam ainda " + anosfaltantes + " anos para tirar seu cartão");
+    Console.WriteLine("Faltam ainda " + diasfaltantes + " dias aproximadamente para tirar seu cartão");
+    idade++;
+    int umanomaisvelho = idade;
+    diasfaltantes = (60 - umanomaisvelho) * 365;
+    Console.WriteLine("Faltaria " + diasfaltantes + " dias aproximadamente para tirar seu cartão se você tivesse um ano mais velho");
+} else
+{
+    Console.WriteLine("Você tem o direito a tirar o cartão idoso da prefeitura");
+}
